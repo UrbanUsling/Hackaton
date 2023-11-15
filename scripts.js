317bfef03e10ef48e9188a8350aeb961f46bdb66
UrbanUsling <UrbanUsling@users.noreply.github.com>
2023-11-15 12:03:28 +0100

    Uppdaterat

--- scripts.js
+++ scripts.js
@@ -16,23 +16,68 @@ function startCountdown() {
         secondsRemaining--;
 
         // Display the updated countdown
-        displayCountdown(secondsRemaining);
+    displayCountdown(secondsRemaining);
 
         // If the countdown reaches 0, show the open website button
     
-            document.getElementById('openWebsiteButton').style.display = 'block';
-            document.getElementById('urlForm').style.display = 'block';
-            console.log("Inne");
+    document.getElementById('uppgift1').classList.remove('hidden');
+    console.log("Inne");
+    if (secondsRemaining === 0) {
+            alert("Times up!");
+        }
     }, 1000);
 }
 
-function submitAnswer(s) {
-    const enteredUrl = document.getElementById('text1').value;
-    if (enteredUrl==='1234'){
-    document.getElementById(s).style.display = 'block';
-    document.getElementById(s).style.display = 'block';
+function submitAnswer(x) {
+
+    if(x==='a'){
+        const enteredPass = document.getElementById('text1').value;
+        if (enteredPass==='12'){
+            alert('Correct!');
+            document.getElementById("uppgift2").classList.remove('hidden');
+        }else{
+            alert('Incorrect!');
+        }
     }
+    if(x==='b'){
+        const enteredPass = document.getElementById('text2').value;
+        if (enteredPass==='34'){
+            alert('Correct!');
+            document.getElementById("uppgift3").classList.remove('hidden');
+        }else{
+            alert('Incorrect!');
+        }
+    }
+    if(x==='c'){
+        const enteredPass = document.getElementById('text3').value;
+        if (enteredPass==='56'){
+            alert('Correct!');
+            document.getElementById("uppgift4").classList.remove('hidden');
+        }else{
+            alert('Incorrect!');
+        }
+    }
+    if(x==='d'){
+        const enteredPass = document.getElementById('text4').value;
+        if (enteredPass==='78'){
+            alert('Correct!');
+            document.getElementById("uppgift5").classList.remove('hidden');
+        }else{
+            alert('Incorrect!');
+        }
+    }
+    if(x==='e'){
+        const enteredPass = document.getElementById('text5').value;
+        if (enteredPass==='90'){
+            alert('Correct! Grattis!!');
+            clearInterval(countdown);
+        }else{
+            alert('Incorrect!');
+        }
+    }
+
 }
+
 
 function displayCountdown(seconds) {
     const hours = Math.floor(seconds / 3600);
